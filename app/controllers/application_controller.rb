c33440f6e3176779865549de0b1d980309e25a6a
Luis Gaytan <gaytan_luis@msn.com>
2015-08-23 14:08:04 -0700

    Assignment 37 completed, flash_attack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :flash_attack
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
+
+  def flash_attack
+    flash[:notice] = "I am a Flash..."
+  end
+
 end
