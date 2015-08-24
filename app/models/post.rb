095a2df517cfa5c8d1379432d429ce71e955b06c
Luis Gaytan <gaytan_luis@msn.com>
2015-08-23 18:33:52 -0700

    Assigment 38 completed, scope added

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,5 +13,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
+  posts = Post.all
+
+  scope :ordered_by_title, -> { posts.order(:title)}
+  #scope :ordered_by_title, -> { order('title ASC')} why will this only work w/o default scope
+  scope :ordered_by_reverse_created_at, -> { posts.order('created_at DESC').reverse}
+  #scope :ordered_by_reverse_created_at, -> { order('created_at ASC' )}
   default_scope { order('created_at DESC') }
+
 end
