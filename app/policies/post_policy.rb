435608be7b1750a26cd0bc710d8c8e07c8cb5e8b
Luis Gaytan <gaytan_luis@msn.com>
2015-08-24 11:19:52 -0700

    Assignment 39 completed, user scope added

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,15 @@ class PostPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  class Scope < Scope
+    def resolve
+      if !user.blank? && (user.admin? || user.moderator?)
+        scope.all
+      else
+        scope.where(user: user)
+      end
+    end
+  end
+
 end
