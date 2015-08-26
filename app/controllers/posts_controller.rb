48051171cbccf005c2c72d29e31e20a6a689f34d
Luis Gaytan <gaytan_luis@msn.com>
2015-08-26 15:22:53 -0700

    Edit was not working. Changed post_controller to fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,23 +4,31 @@ class PostsController < ApplicationController
   #  authorize @posts
   #end
 
-  def show
-    @post = Post.find(params[:id])
-    @topic = Topic.find(params[:topic_id])
-  end
+   def show
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     authorize @post
+   end
 
-  def new
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.new
-    authorize @post
-  end
+   def new
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.new
+     authorize @post
+   end
 
-  def create
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.new(params.require(:post).permit(:title, :body))
-    @post.user = current_user
-    @post.topic = @topic
-    authorize @post
+   def edit
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     authorize @post
+   end
+
+   def create
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.new(params.require(:post).permit(:title, :body))
+     @post.user = current_user
+     @post.topic = @topic
+     authorize @post
+
      if @post.save
        flash[:notice] = "Post was saved."
        redirect_to [@topic, @post]
@@ -28,23 +36,19 @@ class PostsController < ApplicationController
        flash[:error] = "There was an error saving the post. Please try again."
        render :new
      end
-  end
+   end
 
-  def edit
-    @post = Post.find(params[:id])
-    authorize @post
-  end
-
-  def update
-     @post = Post.find(params[:id])
+   def update
      @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
      authorize @post
+
      if @post.update_attributes(params.require(:post).permit(:title, :body))
        flash[:notice] = "Post was updated."
        redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
-       render :edit
+       render :new
      end
    end
-end
+ end
