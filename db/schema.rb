4d282e26650c26b3ba15c9bf10ebdda72e6aa0d3
Luis Gaytan <gaytan_luis@msn.com>
2015-08-27 20:34:54 -0700

    Working on adding Avatar: Modifying the Uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824182927) do
+ActiveRecord::Schema.define(version: 20150828032925) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20150824182927) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
