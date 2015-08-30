9c88202ee84fbd9a6cb1ef6c30f1b2a8a483209a
Luis Gaytan <gaytan_luis@msn.com>
2015-08-30 12:03:00 -0700

    Checkpoint 45 completed, added pagination

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'carrierwave'
 gem 'mini_magick'
 
 gem 'fog'
+
+gem 'will_paginate', '~> 3.0.5'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ users = User.all
 # The `save` method then saves this User to the database.
 
 # Create Topics
-15.times do
+25.times do
   Topic.create!(
   name:        Faker::Lorem.sentence,
   description: Faker::Lorem.paragraph
@@ -30,7 +30,7 @@ end
 topics = Topic.all
 
 # Create Posts
-50.times do
+150.times do
   Post.create!(
     user:   users.sample,
     topic:  topics.sample,
