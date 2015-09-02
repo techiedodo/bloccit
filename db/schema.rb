d31aa0653c7043216c18cbd5726363c9614155d1
Luis Gaytan <gaytan_luis@msn.com>
2015-09-02 10:36:01 -0700

    adjusted size of upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828032925) do
+ActiveRecord::Schema.define(version: 20150831210634) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -26,9 +26,11 @@ ActiveRecord::Schema.define(version: 20150828032925) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150828032925) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
