a5094c84d3617dccce8f1fff10ee1e2bda18ae91
Luis Gaytan <gaytan_luis@msn.com>
2015-09-07 22:02:12 -0700

    changed typo in application.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150824182927) do
 
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -41,14 +33,6 @@ ActiveRecord::Schema.define(version: 20150824182927) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
