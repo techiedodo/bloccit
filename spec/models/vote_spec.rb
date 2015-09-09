f71a4db69b1964d0fefca94b3b95ef915cd26664
Luis Gaytan <gaytan_luis@msn.com>
2015-09-09 08:30:28 -0700

    updated, annotate

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id         :integer          not null, primary key
+#  value      :integer
+#  user_id    :integer
+#  post_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 include TestFactories
