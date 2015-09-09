9e7af611b54dfb3981dfc7e18dfe4f134894b7c1
Luis Gaytan <gaytan_luis@msn.com>
2015-09-08 20:38:47 -0700

    assignment 53 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,18 @@ module ApplicationHelper
     (redcarpet.render markdown).html_safe
   end
 
+  def down_vote_link_classes(post)
+    base = "glyphicon glyphicon-chevron-down "
+    vote = current_user.voted(post)
+    base += "voted" if vote && vote.down_vote?
+    base
+  end
+
+  def up_vote_link_classes(post)
+    base = "glyphicon glyphicon-chevron-up "
+    vote = current_user.voted(post)
+    base += "voted" if vote && vote.up_vote?
+    base
+  end
+
 end
