758ada49af2e75bc020264e944256fcad04a2e3d
Luis Gaytan <gaytan_luis@msn.com>
2015-09-10 10:58:40 -0700

    completed chekpoint 54, public profiles

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,6 +17,14 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   mount_uploader :image, ImageUploader
 
+  default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+
   def up_votes
     votes.where(value: 1).count
   end
@@ -28,13 +36,6 @@ class Post < ActiveRecord::Base
   def points
     votes.sum(:value)
   end
-
-  default_scope { order('rank DESC') }
-
-  validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
 
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / (60*60*24)
