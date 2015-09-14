b81c9af6e17f1b3f74f52e818742df631de568e8
Luis Gaytan <gaytan_luis@msn.com>
2015-09-14 15:32:06 -0700

    section 58 completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,24 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
-    @comment.user_id = current_user.id
+    @comments = @post.comments
+
+    @comment = Comment.new( comment_params )
+    @comment.user = current_user
+    @comment.post = @post
+    @new_comment = Comment.new
+
+    authorize @comment
+
     if @comment.save
-      redirect_to [@post.topic, @post], notice: "Comment saved successfully."
+      flash[:notice] = "Comment was created."
     else
-      redirect_to [@post.topic, @post], notice: "Comment failed to save."
+      flash[:error] = "There was an error saving the comment. Please try again."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
